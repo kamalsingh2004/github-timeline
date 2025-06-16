c174fa083ba8acb85ada640090ed98a2c6ee4b28
kamalsingh2004 <kamalnegi9548@gmail.com>
2025-06-16 10:02:43 +0530

    Update setup_cron.sh

--- src/setup_cron.sh
+++ src/setup_cron.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+# Absolute path to PHP
+PHP_PATH=$(which php)
+
+# Path to your cron.php file
+CRON_FILE_PATH=$(realpath "$(dirname "$0")/cron.php")
+
+# Add CRON entry
+( crontab -l 2>/dev/null; echo "*/5 * * * * $PHP_PATH $CRON_FILE_PATH" ) | crontab -
+
+echo "CRON job set to run every 5 minutes for: $CRON_FILE_PATH"
 
