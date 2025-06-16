21d5eb3ed8e9b97e9821959078de39c0f5955aa9
kamalsingh2004 <kamalnegi9548@gmail.com>
2025-06-16 14:33:17 +0530

    Update cron.php

--- src/cron.php
+++ src/cron.php
@@ -1,5 +1,9 @@
 <?php
-require_once __DIR__ . '/functions.php';
-// Fetch and send GitHub updates
-sendGitHubUpdatesToSubscribers();
+require_once 'functions.php';
 
+// Enable errors (helpful for CRON job debugging)
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Send GitHub timeline updates to all registered users
+sendGitHubUpdatesToSubscribers();
